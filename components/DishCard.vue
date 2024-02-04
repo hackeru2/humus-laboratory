28191fdec4a8d1dd4f8dd182c98c8d0534bd8d68
Amir Lifshitz <amir1004@gmail.com>
2024-02-04 08:33:20 +0200

    slide changed to tabs5 no pic dishcard

--- components/DishCard.vue
+++ components/DishCard.vue
@@ -9,12 +9,12 @@
     max-width="90vw"
     :min-width="carousel ? '90%' : '330px'"
   >
-    <v-img
+    <!-- <v-img
       height="70"
       :lazy-src="props.dish.image"
       :src="props.dish.image"
       class="imag"
-    ></v-img>
+    ></v-img> -->
 
     <v-card color="#303030" elevation="5">
       <v-card-item class="text-center">
