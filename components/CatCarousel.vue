6a565ca746c522d0d4e2e5bebcca72bbf676f5ae
Amir Lifshitz <amir1004@gmail.com>
2024-02-04 10:18:39 +0200

    slide changed to tabs remove windows

--- components/CatCarousel.vue
+++ components/CatCarousel.vue
@@ -50,13 +50,13 @@
       <v-tab :value="2">City</v-tab>
       <v-tab :value="3">Abstract</v-tab>
     </v-tabs> -->
-    <v-window v-model="model">
+    <!-- <v-window v-model="model">
       <v-window-item v-for="(category, n) in items" :key="n" :value="n">
         <v-container fluid>
           <CatDataIterator :category="category" />
         </v-container>
       </v-window-item>
-    </v-window>
+    </v-window> -->
   </v-card>
   <!-- -->
 </template>
